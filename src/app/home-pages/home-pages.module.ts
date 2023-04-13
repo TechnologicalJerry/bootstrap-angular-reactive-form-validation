9bbd65245340ba711dbbdd411cab1a823879ae32
technologicaljerry <cserajatverma@gmail.com>
2023-04-13 21:56:51 +0530

    add require modules

---
+++ src/app/home-pages/home-pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomePagesRoutingModule } from './home-pages-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomePagesRoutingModule
+  ]
+})
+export class HomePagesModule { }
