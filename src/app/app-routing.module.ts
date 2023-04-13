4e94579b145cff455f633302a370a9eafe85b9e9
technologicaljerry <cserajatverma@gmail.com>
2023-04-13 22:10:45 +0530

    implement module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login-pages',
+    loadChildren: () => import('./home-pages/home-pages.module').then(module => module.HomePagesModule)
+  },
+  {
+    path: 'dashboard-pages',
+    loadChildren: () => import('./dashboard-pages/dashboard-pages.module').then(module => module.DashboardPagesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
