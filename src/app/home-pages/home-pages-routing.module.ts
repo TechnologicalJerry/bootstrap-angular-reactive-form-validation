957ff84f36113af8210db66bc49dd0d03cc194df
technologicaljerry <cserajatverma@gmail.com>
2023-04-13 22:20:33 +0530

    Update home-pages-routing.module.ts

--- src/app/home-pages/home-pages-routing.module.ts
+++ src/app/home-pages/home-pages-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('../home-pages/home/home.module').then(module => module.HomeModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('../home-pages/signup/signup.module').then(module => module.SignupModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('../home-pages/login/login.module').then(module => module.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
