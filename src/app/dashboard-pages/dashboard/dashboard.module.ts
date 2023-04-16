ae89aa070b956461418e7ca64fd5fe0784ee0342
technologicaljerry <cserajatverma@gmail.com>
2023-04-16 23:57:02 +0530

    Update dashboard.module.ts

--- src/app/dashboard-pages/dashboard/dashboard.module.ts
+++ src/app/dashboard-pages/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { FormControl } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormControl
   ]
 })
 export class DashboardModule { }
