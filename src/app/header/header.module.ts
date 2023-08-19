b5438192c4a92b6c1ad6d78f9f53b4fc6861522a
technologicaljerry <cserajatverma@gmail.com>
2023-08-19 23:03:13 +0530

    Update header.module.ts

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderRoutingModule } from './header-routing.module';
+import { HeaderComponent } from './header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     HeaderRoutingModule
