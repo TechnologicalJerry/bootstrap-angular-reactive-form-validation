1dfdb77bca45e5d1bc9a91933f50475747e4c31d
technologicaljerry <cserajatverma@gmail.com>
2023-08-20 23:17:57 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
-  { path: 'info', component: SignupComponent, pathMatch: 'full' }
+  { path: '', component: FooterComponent, pathMatch: 'full' }
 
 ];
 
