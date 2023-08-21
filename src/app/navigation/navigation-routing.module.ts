f775cbfadec14ffcc4dcabb3d2f071725e0b2f2c
technologicaljerry <cserajatverma@gmail.com>
2023-08-21 12:42:27 +0530

    Update navigation-routing.module.ts

--- src/app/navigation/navigation-routing.module.ts
+++ src/app/navigation/navigation-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { SignupComponent } from '../signup/signup.component';
+import { FooterComponent } from '../footer/footer.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
+  { path: 'footer', component: FooterComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
