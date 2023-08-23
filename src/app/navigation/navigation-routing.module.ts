625ee2ba6b4dfce503ee9681246c2b3843d0d26b
technologicaljerry <cserajatverma@gmail.com>
2023-08-23 22:46:57 +0530

    Update navigation-routing.module.ts

--- src/app/navigation/navigation-routing.module.ts
+++ src/app/navigation/navigation-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationComponent } from './navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavigationComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
