824d40a4bdeb18bc11c02e941f0a83e29e07c552
technologicaljerry <cserajatverma@gmail.com>
2023-08-24 23:46:47 +0530

    Update navigation.component.ts

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -9,7 +9,7 @@ export class NavigationComponent {
   ngOnInit() {
     var menu_btn = document.querySelector("#menu-btn");
     var sidebar = document.querySelector("#sidebar");
-    var container = document.querySelector(".my-container");
+    // var container = document.querySelector(".my-container");
     // menu_btn.addEventListener("click", () => {
     //   sidebar.classList.toggle("active-nav");
     //   container.classList.toggle("active-cont");
