60179311182a9f30af5c119c74ade173cc5e597b
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-18 23:37:49 +0530

    Create component4.ts

---
+++ src/app/features/dashboard/components/component4/component4.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component4',
+  imports: [],
+  templateUrl: './component4.html',
+  styleUrl: './component4.scss'
+})
+export class Component4 {
+
+}
