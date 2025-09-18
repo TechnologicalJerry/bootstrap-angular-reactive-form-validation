ad61e6f2856de37656686912740236d92a47b30a
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-18 23:37:16 +0530

    Create component1.ts

---
+++ src/app/features/dashboard/components/component1/component1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  imports: [],
+  templateUrl: './component1.html',
+  styleUrl: './component1.scss'
+})
+export class Component1 {
+
+}
