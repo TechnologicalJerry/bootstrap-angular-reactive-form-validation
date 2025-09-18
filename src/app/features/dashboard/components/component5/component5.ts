73c105cfcdb4bb9ed7878ac2784cebc843c879ad
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-18 23:38:00 +0530

    Create component5.ts

---
+++ src/app/features/dashboard/components/component5/component5.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component5',
+  imports: [],
+  templateUrl: './component5.html',
+  styleUrl: './component5.scss'
+})
+export class Component5 {
+
+}
